411d1f963c5bfc381f2341187158851f925f5e97
Maulana Imanulhaq <98022263+Fancyyy21@users.noreply.github.com>
2023-09-25 13:46:05 +0700

    update

--- showImport.js
+++ showImport.js
@@ -1,65 +1,85 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const pointTable = document.getElementById("pointTable").getElementsByTagName('tbody')[0];
 
-    fetch("./data_point.json") // Ganti "data.json" dengan nama file JSON Anda
-        .then(response => response.json())
-        .then(data => {
-            data.features.forEach(feature => {
-                if (feature.geometry.type === "Point") {
-                    const row = pointTable.insertRow();
-                    const nameCell = row.insertCell(0);
-                    const coordinatesCell = row.insertCell(1);
-                    const typeCell = row.insertCell(2);
-                    nameCell.innerText = feature.properties.name;
-                    coordinatesCell.innerText = JSON.stringify(feature.geometry.coordinates);
-                    typeCell.innerText = feature.geometry.type;
-                    
-                }
-            });
-        })
-        .catch(error => console.error("Terjadi kesalahan:", error));
-});
+function addGeoJSONToMapAndTable(geoJSONUrl) {
+    $.getJSON(geoJSONUrl, function () {
 
-document.addEventListener("DOMContentLoaded", () => {
-    const pointTable = document.getElementById("polygonTable").getElementsByTagName('tbody')[0];
+        document.addEventListener("DOMContentLoaded", () => {
+            const pointTable = document.getElementById("pointTable").getElementsByTagName('tbody')[0];
+        
+            fetch("./json/data_point.json") // Ganti "data.json" dengan nama file JSON Anda
+                .then(response => response.json())
+                .then(data => {
+                    data.features.forEach(feature => {
+                        if (feature.geometry.type === "Point") {
+                            const row = pointTable.insertRow();
+                            const nameCell = row.insertCell(0);
+                            const coordinatesCell = row.insertCell(1);
+                            const typeCell = row.insertCell(2);
+                            nameCell.innerText = feature.properties.name;
+                            coordinatesCell.innerText = JSON.stringify(feature.geometry.coordinates);
+                            typeCell.innerText = feature.geometry.type;
+                            
+                        }
+                    });
+                })
+                .catch(error => console.error("Terjadi kesalahan:", error));
+        });
+        
+        document.addEventListener("DOMContentLoaded", () => {
+            const pointTable = document.getElementById("polygonTable").getElementsByTagName('tbody')[0];
+        
+            fetch("./json/data_polygon.json") // Ganti "data.json" dengan nama file JSON Anda
+                .then(response => response.json())
+                .then(data => {
+                    data.features.forEach(feature => {
+                        if (feature.geometry.type === "Polygon") {
+                            const row = pointTable.insertRow();
+                            const nameCell = row.insertCell(0);
+                            const coordinatesCell = row.insertCell(1);
+                            const typeCell = row.insertCell(2);
+                            nameCell.innerText = feature.properties.name;
+                            coordinatesCell.innerText = JSON.stringify(feature.geometry.coordinates);
+                            typeCell.innerText = feature.geometry.type;
+                            
+                        }
+                    });
+                })
+                .catch(error => console.error("Terjadi kesalahan:", error));
+        });
+        
+        document.addEventListener("DOMContentLoaded", () => {
+            const pointTable = document.getElementById("polylineTable").getElementsByTagName('tbody')[0];
+        
+            fetch("./json/data_polyline.json") // Ganti "data.json" dengan nama file JSON Anda
+                .then(response => response.json())
+                .then(data => {
+                    data.features.forEach(feature => {
+                        if (feature.geometry.type === "LineString") {
+                            const row = pointTable.insertRow();
+                            const nameCell = row.insertCell(0);
+                            const coordinatesCell = row.insertCell(1);
+                            const typeCell = row.insertCell(2);
+                            nameCell.innerText = feature.properties.name;
+                            coordinatesCell.innerText = JSON.stringify(feature.geometry.coordinates);
+                            typeCell.innerText = feature.geometry.type;
+                            
+                        }
+                    });
+                })
+                .catch(error => console.error("Terjadi kesalahan:", error));
+        });
 
-    fetch("./data_polygon.json") // Ganti "data.json" dengan nama file JSON Anda
-        .then(response => response.json())
-        .then(data => {
-            data.features.forEach(feature => {
-                if (feature.geometry.type === "Polygon") {
-                    const row = pointTable.insertRow();
-                    const nameCell = row.insertCell(0);
-                    const coordinatesCell = row.insertCell(1);
-                    const typeCell = row.insertCell(2);
-                    nameCell.innerText = feature.properties.name;
-                    coordinatesCell.innerText = JSON.stringify(feature.geometry.coordinates);
-                    typeCell.innerText = feature.geometry.type;
-                    
-                }
-            });
-        })
-        .catch(error => console.error("Terjadi kesalahan:", error));
-});
+    }
 
-document.addEventListener("DOMContentLoaded", () => {
-    const pointTable = document.getElementById("polylineTable").getElementsByTagName('tbody')[0];
+    )
+}
 
-    fetch("./data_polyline.json") // Ganti "data.json" dengan nama file JSON Anda
-        .then(response => response.json())
-        .then(data => {
-            data.features.forEach(feature => {
-                if (feature.geometry.type === "LineString") {
-                    const row = pointTable.insertRow();
-                    const nameCell = row.insertCell(0);
-                    const coordinatesCell = row.insertCell(1);
-                    const typeCell = row.insertCell(2);
-                    nameCell.innerText = feature.properties.name;
-                    coordinatesCell.innerText = JSON.stringify(feature.geometry.coordinates);
-                    typeCell.innerText = feature.geometry.type;
-                    
-                }
-            });
-        })
-        .catch(error => console.error("Terjadi kesalahan:", error));
-});
+// Call the function for LineString GeoJSON using the raw URL
+addGeoJSONToMapAndTable('https://raw.githubusercontent.com/Fancyyy21/maulana123.github.io/main/json/data_polyline.json', document.querySelector('table'));
+
+// Call the function for Polygon GeoJSON
+addGeoJSONToMapAndTable('https://raw.githubusercontent.com/Fancyyy21/maulana123.github.io/main/json/data_polygon.json', document.querySelector('table'));
+
+// Call the function for Point GeoJSON
+addGeoJSONToMapAndTable('https://raw.githubusercontent.com/Fancyyy21/maulana123.github.io/main/json/data_point.json', document.querySelector('table'));
+
+
